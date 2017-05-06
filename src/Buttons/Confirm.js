ef5c03dd5c0cf5453eec6c281d5d1a43b3a3dd27
jdarling <jeremy.darling@gmail.com>
2017-05-05 21:54:55 -0500

    Fix ConfirmDialog import

--- src/Buttons/Confirm.js
+++ src/Buttons/Confirm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Button from './Button';
-import {ConfirmDialog} from '../Dialogs';
+import ConfirmDialog from '../Dialogs/Confirm';
 
 class ConfirmButton extends React.Component{
   constructor({dialogOpen=false}){
