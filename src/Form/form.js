8586c2ad06251e5a7c34e29e16080e7a0c40171f
jdarling <jeremy.darling@gmail.com>
2017-05-10 14:53:47 -0500

    Allow data or formData prop for data

--- src/Form/form.js
+++ src/Form/form.js
@@ -9,12 +9,15 @@ class Form extends Component{
       children,
       schemaEncoder,
       schema: formSchema,
+      formData,
+      data,
       ...props
     } = this.props;
     const schema = schemaEncoder?schemaEncoder(formSchema):formSchema;
     return (
       <JsonSchemaForm
         schema={schema}
+        formData={formData||data}
         {...props}
         >
         {children}
@@ -24,6 +27,7 @@ class Form extends Component{
 };
 
 Form.propTypes = Object.assign({}, JsonSchemaForm.propTypes, {
+  data: PropTypes.oneOfType(PropTypes.object, PropTypes.array),
   schemaEncoder: PropTypes.func
 });
 
