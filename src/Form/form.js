891f99db7607cba568ac967c51eca03055215506
jdarling <jeremy.darling@gmail.com>
2017-05-10 22:11:59 -0500

    Initial work on submit

--- src/Form/form.js
+++ src/Form/form.js
@@ -1,9 +1,38 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import {
+  fetchJson
+} from 'martingale-utils';
 
 import JsonSchemaForm from 'react-jsonschema-form';
 
-class Form extends Component{
+class BaseForm extends Component{
+  onSubmit({formData}){
+    const {
+      successUrl,
+      submitTo,
+      history,
+      mapper,
+      onSubmit
+    } = this.props;
+    const data = mapper?mapper(formData):formData;
+    if(onSubmit){
+      return onSubmit(data);
+    }
+    if(submitTo){
+      const {
+        method = 'POST',
+        url
+      } = typeof(submitTo)==='string'?{url: submitTo}:submitTo;
+      // do something with fetch here
+    }
+    if(typeof(successUrl)==='function'){
+      return history.push(successUrl(data));
+    }
+    history.push(successUrl||'/');
+  }
+
   render(){
     const {
       children,
@@ -18,6 +47,7 @@ class Form extends Component{
       <JsonSchemaForm
         schema={schema}
         formData={formData||data}
+        onSubmit={this.onSubmit.bind(this)}
         {...props}
         >
         {children}
@@ -26,10 +56,16 @@ class Form extends Component{
   }
 };
 
+const Form = withRouter(BaseForm);
+
 Form.propTypes = Object.assign({}, JsonSchemaForm.propTypes, {
   schema: PropTypes.object,
   data: PropTypes.oneOfType(PropTypes.object, PropTypes.array),
-  schemaEncoder: PropTypes.func
+  schemaEncoder: PropTypes.func,
+  successUrl: PropTypes.oneOfType(PropTypes.string, PropTypes.func),
+  submitTo: PropTypes.oneOfType(PropTypes.string, PropTypes.object),
+  mapper: PropTypes.func,
+  onSubmit: PropTypes.func
 });
 
 export default Form;
